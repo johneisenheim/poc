41879b8a2cfc2738103db5b55a30cf296e89d00d
Nello Saulino <marco.beatrice@sky.it>
2017-11-21 14:25:32 +0100

    Socketio

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import io from 'socket.io'
+import socketIO from 'socket.io'
 import http from 'http'
 import express from 'express'
 
@@ -6,6 +6,14 @@ import express from 'express'
 let app = express();
 let server = http.Server(app);
 let port = process.env.PORT || 4000;
+let io = new socketIO(server)
+
+io.on('connection', (socket) => {
+    // io.emit('userJoin', {nick: currentUser.nick});
+    // socket.on('ding', () => {
+    //     socket.emit('dong');
+    // });
+})
 
 
 server.listen(port, () => {
